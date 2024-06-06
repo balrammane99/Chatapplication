8bdfb3a2730effb9d66dd61d5007f45b2bf13a1a
balrammane99 <balrammane3307@gmail.com>
2024-06-06 10:36:56 +0530

    fourth changes

--- app/src/main/java/com/example/chatapplication/MainActivity.java
+++ app/src/main/java/com/example/chatapplication/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         //changes 2
         //changes 3
         //changes 4
+        //changes 4
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
